39eb0f937134098a5905fe06f06fc4513fdc3568
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-12-01 11:59:37 -0700

    :recycle: Make Day 1 faster and cleaner

--- python/day1.py
+++ python/day1.py
@@ -25,17 +25,30 @@ def part1(inp):
     return sum
 
 
+def __find_number(line, start):
+    if line[start].isnumeric():
+        return int(line[start])
+
+    for number in string_numbers:
+        if number in line[start : start + len(number)]:
+            return string_numbers[number]
+
+    return None
+
+
 def part2(inp):
     sum = 0
     for line in inp:
         numbers = []
         for start in range(0, len(line)):
-            if line[start].isnumeric():
-                numbers.append(int(line[start]))
-                continue
-            for number in string_numbers:
-                if number in line[start : start + len(number)]:
-                    numbers.append(string_numbers[number])
+            if n := __find_number(line, start):
+                numbers.append(n)
+                break
+
+        for start in range(len(line) - 1, 0, -1):
+            if n := __find_number(line, start):
+                numbers.append(n)
+                break
 
         sum += numbers[0] * 10 + numbers[-1]
     return sum
